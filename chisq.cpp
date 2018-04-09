832de6c6b240b7cc10e591d9feeb06653c0e3016
JosephBingham <joebingham@me.com>
2018-04-08 20:35:40 -0500

    working

--- chisq.cpp
+++ chisq.cpp
@@ -1,20 +1,39 @@
 #include<iostream>
 #include<stdlib.h>
 #include<math.h>
+#ifndef SIZE
 #define SIZE 256
+#endif
+using namespace std;
+const double PI = 3.1415926535897932384626433832795028841971693993751058209749445923;
+const double E = 2.7182818284590452353602874713526624977572470936999595749669676277;
 
-double chi2cdf(double sum, double k)
+double gamm(double x) 
 {
-	return sum + k;	
+    double ret = (1.000000000190015 + 
+                 76.18009172947146 / (x + 1) +  
+                 -86.50532032941677 / (x + 2) + 
+                 24.01409824083091 / (x + 3) +  
+                 -1.231739572450155 / (x + 4) + 
+                 1.208650973866179e-3 / (x + 5) + 
+                 -5.395239384953e-6 / (x + 6));
+    
+    return ret * sqrt(2*PI)/x * pow(x + 5.5, x+.5) * exp(-x-5.5);
 }
 
+double chisqrcdf(double x, double n) {
+	double a = 1/(gamm(n/2.0)*pow(2.0, n/2.0));
+	double b = pow(x, (n/2.0) - 1.0);
+	double c = pow(E, -x/2.0);
+	return a*b*c;
+}
 
-double chisq(int B[SIZE][SIZE])
+double chisq(int B[SIZE])
 {
 	int counts[SIZE] = {0};
 	for ( int i = 0; i < SIZE; i++)
 	{
-		counts[B[i][1] + 1]++;
+		counts[B[i] + 1]++;
 	}
 	double hist1[128] = {0};
 	double hist2[128] = {0};
@@ -49,10 +68,10 @@ double chisq(int B[SIZE][SIZE])
 	{
 		sum += (pow(hist1[i] + hist2[i], 2))/hist2[i];
 	}
-	return 1 - (chi2cdf(sum,k-1));
+	return 1 - (chisqrcdf(sum,k-1));
 }
 
 int main(void)
 {
-	return 0;
+	cout << chisqrcdf(2,2) << endl;
 }
