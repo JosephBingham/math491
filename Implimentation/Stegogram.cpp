cfd234f6f62f0763cbdd5d7d00245a1dd7ba0d02
JosephBingham <joebingham@me.com>
2018-04-14 15:28:17 -0500

    working

--- Implimentation/Stegogram.cpp
+++ Implimentation/Stegogram.cpp
@@ -1,13 +1,25 @@
 #include<iostream>
 #include<stdlib.h>
 #include<math.h>
+using namespace std;
+
 #ifndef SIZE
 #define SIZE 256
 #endif
-using namespace std;
-const double PI = 3.1415926535897932384626433832795028841971693993751058209749445923;
-const double E = 2.7182818284590452353602874713526624977572470936999595749669676277;
 
+#ifndef IMG_SIZE
+#define IMG_SIZE 1024
+#endif
+const double PI = 3.1415926535897932384626433832795028841971693993751058209749445923;
+const double E  = 2.7182818284590452353602874713526624977572470936999595749669676277;
+FILE *file = NULL;
+
+/*
+	gamm:
+	param: double x, input to function
+	return: double, the gamma function applied
+	implimented using an aproximations
+*/
 double gamm(double x) 
 {
     double ret = (1.000000000190015 + 
@@ -15,12 +27,18 @@ double gamm(double x)
                  -86.50532032941677 / (x + 2) + 
                  24.01409824083091  / (x + 3) +  
                  -1.231739572450155 / (x + 4) + 
-                 .001208650973866179/ (x + 5) + 
-                 -.00005395239384953 / (x + 6));
+                 1.208650973866179e-3/ (x + 5) + 
+                 -5.395239384953e-5 / (x + 6));
     
     return ret * sqrt(2.0*PI)/x * pow(x + 5.5, x+.5) * exp(-x-5.5);
 }
 
+/*
+	chisqrcdf:
+	param: double x, value to test against
+		   double n, degree of freedom
+	return: double, the chi square cdf
+*/
 double chisqrcdf(double x, double n) {
 	double a = 1/(gamm(n/2.0)*pow(2.0, n/2.0));
 	double b = pow(x, (n/2.0) - 1.0);
@@ -29,7 +47,12 @@ double chisqrcdf(double x, double n) {
 	return a*b*c;
 }
 
-double chisq(int B[SIZE])
+/*
+	fitness:
+	param: int *B, size of SIZE, the model
+
+*/
+double fitness(int B[SIZE])
 {
 	int counts[SIZE] = {0};
 	for ( int i = 0; i < SIZE; i++)
@@ -72,7 +95,37 @@ double chisq(int B[SIZE])
 	return 1 - (chisqrcdf(sum,k-1));
 }
 
-int main(void)
+typedef struct values
 {
+	int idx;
+	int val;
+} VAL_T;
+
+class solution
+{
+	private:
+		VAL_T **values;
+		double fitVal;
+	
+	public:
+		void solution()
+		{	
+			VAL_T  **values = (VAL_T **)malloc(4*sizeof(VAL_T *));
+			for(int i = 0; i < 4; i++)
+			{
+				values[i] = (VAL_T *)malloc(IMG_SIZE/4 *sizeof(VAL_T))
+			}
+		}
+		void mixSolutions(solution other)
+		{
+			
+		}
+}
+
+
+int main(int argc, char *argv[])
+{
+
+
 	cout << chisqrcdf(3,3) << endl;
 }
